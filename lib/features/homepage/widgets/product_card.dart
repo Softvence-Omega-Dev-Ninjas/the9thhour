b0ac4eb69b30c057b1599c87e06b351b8c2623f1
NayanSoftDev <noyon040597@gmail.com>
2026-01-22 04:54:00 +0600

    update screen

--- lib/features/homepage/widgets/product_card.dart
+++ lib/features/homepage/widgets/product_card.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:the9thhour/core/common/constants/iconpath.dart';
 import 'package:the9thhour/core/common/style/global_text_style.dart';
 import 'package:the9thhour/core/common/widgets/custom_secondary_button.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProductCard extends StatefulWidget {
   final String name;
@@ -89,16 +94,19 @@ class _ProductCardState extends State<ProductCard> {
                 Positioned(
                   top: 9.h,
                   right: 12.w,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Color(0xFF5E3E7E),
-                      shape: BoxShape.circle,
-                    ),
-                    padding: EdgeInsets.all(8.w),
-                    child: Image(
-                      image: AssetImage(IconPath.shareIcon),
-                      width: 16.w,
-                      height: 16.h,
+                  child: GestureDetector(
+                    onTap: () => _showShareBottomSheet(context),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF5E3E7E),
+                        shape: BoxShape.circle,
+                      ),
+                      padding: EdgeInsets.all(8.w),
+                      child: Image(
+                        image: AssetImage(IconPath.shareIcon),
+                        width: 16.w,
+                        height: 16.h,
+                      ),
                     ),
                   ),
                 ),
@@ -242,5 +250,183 @@ class _ProductCardState extends State<ProductCard> {
         ],
       ),
     );
+  }
+
+  void _showShareBottomSheet(BuildContext context) {
+    final String shareText = 'Check out ${widget.name} by ${widget.brand}!';
+
+    Get.bottomSheet(
+      DraggableScrollableSheet(
+        initialChildSize: 0.45,
+        minChildSize: 0.3,
+        maxChildSize: 0.9,
+        builder: (BuildContext context, ScrollController scrollController) {
+          return Container(
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xFFFFCFE9), Color(0xFFF5E6F0)],
+              ),
+              borderRadius: BorderRadius.vertical(top: Radius.circular(24.r)),
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: SingleChildScrollView(
+              controller: scrollController,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(height: 12.h),
+                  Container(
+                    width: 100,
+                    height: 4,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 189, 153, 153),
+                      borderRadius: BorderRadius.circular(10.r),
+                    ),
+                  ),
+                  SizedBox(height: 24.h),
+                  Text(
+                    'Share With Friends',
+                    style: GlobalTextStyle.heading2.copyWith(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  GridView.count(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 15,
+                    crossAxisSpacing: 6,
+                    childAspectRatio: 0.8,
+                    children: [
+                      _buildShareOption(
+                        icon: Icons.copy,
+                        label: 'Copy Link',
+                        color: Colors.grey.shade700,
+                        onTap: () async {
+                          await Clipboard.setData(
+                            ClipboardData(text: shareText),
+                          );
+                          Get.back();
+                          Get.snackbar(
+                            'Success',
+                            'Link copied to clipboard',
+                            snackPosition: SnackPosition.BOTTOM,
+                            margin: EdgeInsets.all(16.w),
+                          );
+                        },
+                      ),
+                      _buildShareOption(
+                        icon: FontAwesomeIcons.whatsapp,
+                        label: 'WhatsApp',
+                        color: const Color(0xFF25D366),
+                        onTap: () =>
+                            _launchUrl('whatsapp://send?text=$shareText'),
+                      ),
+                      _buildShareOption(
+                        icon: FontAwesomeIcons.facebook,
+                        label: 'Facebook',
+                        color: const Color(0xFF1877F2),
+                        onTap: () => _launchUrl(
+                          'https://www.facebook.com/sharer/sharer.php?u=example.com&quote=$shareText',
+                        ),
+                      ),
+                      _buildShareOption(
+                        icon: FontAwesomeIcons.facebookMessenger,
+                        label: 'Messenger',
+                        color: const Color(0xFF006AFF),
+                        onTap: () => _launchUrl(
+                          'fb-messenger://share/?link=example.com',
+                        ),
+                      ),
+                      _buildShareOption(
+                        icon: FontAwesomeIcons.twitter,
+                        label: 'Twitter',
+                        color: const Color(0xFF1DA1F2),
+                        onTap: () => _launchUrl(
+                          'https://twitter.com/intent/tweet?text=$shareText',
+                        ),
+                      ),
+                      _buildShareOption(
+                        icon: FontAwesomeIcons.instagram,
+                        label: 'Instagram',
+                        color: const Color(0xFFE4405F),
+                        onTap: () => _launchUrl('instagram://share'),
+                      ),
+                      _buildShareOption(
+                        icon: FontAwesomeIcons.skype,
+                        label: 'Skype',
+                        color: const Color(0xFF00AFF0),
+                        onTap: () => _launchUrl('skype:?chat&topic=$shareText'),
+                      ),
+                      _buildShareOption(
+                        icon: Icons.message,
+                        label: 'Message',
+                        color: const Color(0xFF5E3E7E),
+                        onTap: () => Share.share(shareText),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 24.h),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
+    );
+  }
+
+  Widget _buildShareOption({
+    required IconData icon,
+    required String label,
+    required Color color,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 50.w,
+            height: 50.w,
+            decoration: BoxDecoration(shape: BoxShape.circle),
+            child: Center(
+              child: Icon(icon, color: color, size: 24.sp),
+            ),
+          ),
+          SizedBox(height: 8.h),
+          Text(
+            label,
+            textAlign: TextAlign.center,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: GlobalTextStyle.bodyText.copyWith(
+              fontSize: 11.sp,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _launchUrl(String urlString) async {
+    Get.back();
+    final Uri url = Uri.parse(urlString);
+    try {
+      if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+        // Fallback to generic share if specific app launch fails
+        Share.share('Check out ${widget.name} by ${widget.brand}!');
+      }
+    } catch (e) {
+      Share.share('Check out ${widget.name} by ${widget.brand}!');
+    }
   }
 }
